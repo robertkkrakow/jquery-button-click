61112e9b7a696a6f69f8913837b58beb2d216744
Robert Kowal <robertk.krakow@gmail.com>
2017-06-25 16:29:54 +0200

    end

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+$(document).ready(function () {
+            $("button").click(function () {
+                $.getJSON("http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl", function (data) {
+                    console.log(data);
+                });
+
+            });
+});
